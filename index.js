711236f26d0dd64b7acc01e609ea139c0371a710
jaysingh09 <jay09.singh@gmail.com>
2018-03-13 23:57:04 +0530

    Simplifying base

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// Dependencies
+var express = require('express');
+// var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// MongoDB
+
+// mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/products-demo');
+// mongoose.connection.on('error', function(){});
+
+// Express
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', require('./server/routes/app.route'));
+
+// Start server
+var port = process.env.OPENSHIFT_NODEJS_PORT || 5000
+  , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+app.listen(port, ip, function () {
+  console.log('Express server listening on %d', port);
+});
